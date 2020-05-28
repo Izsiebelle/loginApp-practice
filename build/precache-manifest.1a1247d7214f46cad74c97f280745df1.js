11f5a7594af4976d089323668da91715e0f3b568
izsie <izsie.robinson@gmail.com>
2020-05-28 15:47:07 -0500

    login application practice

---
+++ build/precache-manifest.1a1247d7214f46cad74c97f280745df1.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c4a9e12d1748048bc101edfa62a321d5",
+    "url": "./index.html"
+  },
+  {
+    "revision": "dcdf40c0a81e0d8016fc",
+    "url": "./static/css/main.979c2c2c.chunk.css"
+  },
+  {
+    "revision": "84fe96e0468ca7b4bb52",
+    "url": "./static/js/2.86dcea27.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "./static/js/2.86dcea27.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "dcdf40c0a81e0d8016fc",
+    "url": "./static/js/main.d9d89001.chunk.js"
+  },
+  {
+    "revision": "6ff08f4d2bb391b3889e",
+    "url": "./static/js/runtime-main.2ef79076.js"
+  }
+]);
